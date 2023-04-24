997a580eb937d7601f5c044993a2b6a64eb5559c
hihithisisme <soong.lexuan6@gmail.com>
2023-04-24 10:42:56 +0800

    feat: spring initializr

---
+++ src/main/java/com/partior/reconmission/ReconMissionApplication.java
@@ -0,0 +1,13 @@
+package com.partior.reconmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconMissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partior/reconmission/ReconMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partior.reconmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
